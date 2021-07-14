f67857d4bf98054abdbc80015c2def6f121bb87d
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-13 22:27:35 -0600

    Add dish class and attributes tests

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,11 +1,21 @@
 require './lib/dish'
 
 RSpec.describe Dish do
+  before(:each) do
+    @dish = Dish.new("Couscous Salad", :appetizer)
+  end
   describe 'dish' do
     it 'is a dish' do
-      dish = Dish.new("Couscous Salad", :appetizer)
+      # dish = Dish.new("Couscous Salad", :appetizer)
+      expect(@dish).to be_a(Dish)
+    end
 
-      expect(dish).to be_a(Dish)
+    it 'has a name' do
+      expect(@dish.name).to eq("Couscous Salad")
+    end
+
+    it 'has a category' do
+      expect(@dish.category).to eq(:appetizer)
     end
   end
 end
