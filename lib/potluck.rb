115f6ee67c8d4baa1e53099f6922e645074aeb29
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-14 00:41:59 -0600

    Create menu method currently outputs hash; keys sorted, values not

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,4 +1,4 @@
-require  './lib/dish'
+require  './dish'
 
 class Potluck
   attr_reader :date, :dishes
@@ -6,6 +6,7 @@ class Potluck
   def initialize(date)
     @date = date
     @dishes = []
+    @menu = Hash.new
   end
 
   def add_dish(dish)
@@ -17,4 +18,34 @@ class Potluck
       dish.category == category
     end
   end
-end
+
+  def menu
+    categories = []
+    @dishes.each do |dish|
+      categories << dish.category
+    end
+
+    categories.uniq.sort.each do |category|
+      @menu[category] = get_all_from_category(category)
+    end
+    @menu
+    end
+  end
+
+
+
+# potluck = Potluck.new("7-13-18")
+# couscous_salad = Dish.new("Couscous Salad", :appetizer)
+# cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+# summer_pizza = Dish.new("Summer Pizza", :appetizer)
+# roast_pork = Dish.new("Roast Pork", :entre)
+# candy_salad = Dish.new("Candy Salad", :dessert)
+# bean_dip = Dish.new("Bean Dip", :appetizer)
+#
+# potluck.add_dish(couscous_salad)
+# potluck.add_dish(summer_pizza)
+# potluck.add_dish(roast_pork)
+# potluck.add_dish(cocktail_meatballs)
+# potluck.add_dish(candy_salad)
+# potluck.add_dish(bean_dip)
+# puts potluck.menu
