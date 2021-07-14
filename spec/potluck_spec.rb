90c31b84cb5266739e6e2608e59cf3aa71932357
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-13 23:40:27 -0600

    Add get_all_from_category method tests

--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Potluck do
     @potluck = Potluck.new("7-13-18")
     @couscous_salad = Dish.new("Couscous Salad", :appetizer)
     @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    @roast_pork = Dish.new("Roast Pork", :entre)
+    @candy_salad = Dish.new("Candy Salad", :dessert)
+
   end
 
   describe 'potluck' do
@@ -34,6 +38,18 @@ RSpec.describe Potluck do
       @potluck.add_dish(@cocktail_meatballs)
 
       expect(@potluck.dishes.length).to eq(2)
+    end
+
+    it 'collects dish objects by category in array' do
+      @potluck.add_dish(@couscous_salad)
+      @potluck.add_dish(@summer_pizza)
+      @potluck.add_dish(@roast_pork)
+      @potluck.add_dish(@cocktail_meatballs)
+      @potluck.add_dish(@candy_salad)
+
+      expect(@potluck.get_all_from_category(:appetizer)).to eq([@couscous_salad, @summer_pizza])
+      expect(@potluck.get_all_from_category(:appetizer).first).to eq(@couscous_salad)
+      expect(@potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
     end
 
   end
